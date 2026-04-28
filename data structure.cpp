377c15ef52619046cb315812bbc664e690890f7e
Galal <159665895+GalalHussein@users.noreply.github.com>
2026-04-28 14:36:33 +0300

    منكش الحناكش
    Restored commented-out code for various data structures and algorithms.

--- data structure.cpp
+++ data structure.cpp
@@ -3,151 +3,153 @@ namespace min_heap {
 // using min_heap = priority_queue<U, vector<U>, greater<U>>;
 }
 namespace SegTree {
-    /*
-struct segTree {
-#define L(x) (2 * x + 1)
-#define R(x) (2 * x + 2)
-int size;
-const int init = 1e18;
-vector<int> tree;
-segTree(int n) {
-size = 1uLL << (64 - __builtin_clzll(n - 1));
-tree.assign(2 * size, init);
+    // struct segTree {
+    // #define L(x) (2 * x + 1)
+    // #define R(x) (2 * x + 2)
+    // int size;
+    // const int init = 1e18;
+    // vector<int> tree;
+    // segTree(int n) {
+    // size = 1uLL << (64 - __builtin_clzll(n - 1));
+    // tree.assign(2 * size, init);
+    // }
+    // int merge (int __left, int __right) {
+    // return min(__left, __right);
+    // }
+    // void update (int i, int v, int x, int lx, int rx) {
+    // if (rx - lx == 1) {
+    //     tree[x] = v;
+    //     return;
+    // }
+    // int mid = (lx + rx) / 2;
+    // if (i < mid) {
+    //     update (i, v, L(x), lx, mid);
+    // } else {
+    //     update (i, v, R(x), mid, rx);
+    // }
+    // tree[x] = merge (tree[L(x)], tree[R(x)]);
+    // }
+    //
+    // void update (int i, int v) {
+    // update (i, v, 0, 0, size);
+    // }
+    //
+    // int query (int l, int r, int x, int lx, int rx) {
+    // if (l >= rx or r <= lx) return init;
+    // if (lx >= l && rx <= r) return tree[x];
+    //
+    // int mid = (lx + rx) / 2;
+    // return merge (query (l, r, L(x), lx, mid),query (l, r, R(x), mid, rx));
+    // }
+    //
+    // int query (int l, int r) {
+    // return query(l, r, 0, 0, size);
+    // }
+    //
+    // #undef L
+    // #undef R
+    // };
+    //
+    // }
 }
-int merge (int __left, int __right) {
-return min(__left, __right);
-}
-void update (int i, int v, int x, int lx, int rx) {
-if (rx - lx == 1) {
-    tree[x] = v;
-    return;
-}
-int mid = (lx + rx) / 2;
-if (i < mid) {
-    update (i, v, L(x), lx, mid);
-} else {
-    update (i, v, R(x), mid, rx);
-}
-tree[x] = merge (tree[L(x)], tree[R(x)]);
-}
+namespace SegTree2Max {
+//     // struct Max2SegTree {
+//     //     struct Node { long long mx, smax; };
+//     //     int n;
+//     //     vector<Node> st;
+//     //     Max2SegTree() = default;
+//     //     Max2SegTree(const vector<long long>& a){
+//     //         n=(int)a.size();
+//     //         st.assign(4*n, {LLONG_MIN, LLONG_MIN});
+//     //         build(1,0,n-1,a);
+//     //     }
+//     //     Node merge(const Node& A,const Node& B){
+//     //         Node R;
+//     //         if(A.mx>=B.mx){ R.mx=A.mx; R.smax=max(A.smax, B.mx); }
+//     //         else { R.mx=B.mx; R.smax=max(B.smax, A.mx); }
+//     //         return R;
+//     //     }
+//     //     void build(int p,int l,int r,const vector<long long>& a){
+//     //         if(l==r){ st[p]={a[l], LLONG_MIN}; return; }
+//     //         int m=(l+r)/2;
+//     //         build(p<<1,l,m,a);
+//     //         build(p<<1|1,m+1,r,a);
+//     //         st[p]=merge(st[p<<1],st[p<<1|1]);
+//     //     }
+//     //     Node query(int ql,int qr){ return query(1,0,n-1,ql,qr); }
+//     //     Node query(int p,int l,int r,int ql,int qr){
+//     //         if(ql>r||qr<l) return {LLONG_MIN, LLONG_MIN};
+//     //         if(ql<=l&&r<=qr) return st[p];
+//     //         int m=(l+r)/2;
+//     //         return merge(query(p<<1,l,m,ql,qr), query(p<<1|1,m+1,r,ql,qr));
+//     //     }
+//     //     void point_update(int idx,long long v){ point_update(1,0,n-1,idx,v); }
+//     //     void point_update(int p,int l,int r,int idx,long long v){
+//     //         if(l==r){ st[p]={v,LLONG_MIN}; return; }
+//     //         int m=(l+r)/2;
+//     //         if(idx<=m) point_update(p<<1,l,m,idx,v); else point_update(p<<1|1,m+1,r,idx,v);
+//     //         st[p]=merge(st[p<<1],st[p<<1|1]);
+//     //     }
+//     // };
 
-void update (int i, int v) {
-update (i, v, 0, 0, size);
-}
-
-int query (int l, int r, int x, int lx, int rx) {
-if (l >= rx or r <= lx) return init;
-if (lx >= l && rx <= r) return tree[x];
-
-int mid = (lx + rx) / 2;
-return merge (query (l, r, L(x), lx, mid),query (l, r, R(x), mid, rx));
-}
-
-int query (int l, int r) {
-return query(l, r, 0, 0, size);
-}
-
-#undef L
-#undef R
-};
-struct Max2SegTree {
-struct Node { long long mx, smax; };
-int n;
-vector<Node> st;
-Max2SegTree() = default;
-Max2SegTree(const vector<long long>& a){
-n=(int)a.size();
-st.assign(4*n, {LLONG_MIN, LLONG_MIN});
-build(1,0,n-1,a);
-}
-Node merge(const Node& A,const Node& B){
-Node R;
-if(A.mx>=B.mx){ R.mx=A.mx; R.smax=max(A.smax, B.mx); }
-else { R.mx=B.mx; R.smax=max(B.smax, A.mx); }
-return R;
-}
-void build(int p,int l,int r,const vector<long long>& a){
-if(l==r){ st[p]={a[l], LLONG_MIN}; return; }
-int m=(l+r)/2;
-build(p<<1,l,m,a);
-build(p<<1|1,m+1,r,a);
-st[p]=merge(st[p<<1],st[p<<1|1]);
-}
-Node query(int ql,int qr){ return query(1,0,n-1,ql,qr); }
-Node query(int p,int l,int r,int ql,int qr){
-if(ql>r||qr<l) return {LLONG_MIN, LLONG_MIN};
-if(ql<=l&&r<=qr) return st[p];
-int m=(l+r)/2;
-return merge(query(p<<1,l,m,ql,qr), query(p<<1|1,m+1,r,ql,qr));
-}
-void point_update(int idx,long long v){ point_update(1,0,n-1,idx,v); }
-void point_update(int p,int l,int r,int idx,long long v){
-if(l==r){ st[p]={v,LLONG_MIN}; return; }
-int m=(l+r)/2;
-if(idx<=m) point_update(p<<1,l,m,idx,v); else point_update(p<<1|1,m+1,r,idx,v);
-st[p]=merge(st[p<<1],st[p<<1|1]);
-}
-};
-
-*/
 }
 namespace MergeSortTree {
-// 0-index and Not Include R
-/*
-    struct MergeSortTrees {
-        int size;
-        vector<vector<ll>> tree;
-        explicit MergeSortTrees(const vector<ll>& a) {
-            size = a.size();
-            tree.resize(4 * size);
-            build(0, 0, size, a);
-        }
-        void build(int x, int lx, int rx, const vector<ll>& a) {
-            if (rx - lx == 1) {
-                tree[x] = {a[lx]};
-                return;
-            }
-            int mid = (lx + rx) / 2;
-            build(2 * x + 1, lx, mid, a);
-            build(2 * x + 2, mid, rx, a);
-            tree[x].resize(tree[2 * x + 1].size() + tree[2 * x + 2].size());
-            merge(tree[2 * x + 1].begin(), tree[2 * x + 1].end(),
-                 tree[2 * x + 2].begin(), tree[2 * x + 2].end(),
-                 tree[x].begin());
-        }
-        int lessOnly(int l, int r, ll v, int x, int lx, int rx) {
-            if (lx >= r || rx <= l) return 0;
-            if (lx >= l && rx <= r) {
-                return int(lower_bound(tree[x].begin(), tree[x].end(), v) - tree[x].begin());
-            }
-            int mid = (lx + rx) / 2;
-            return lessOnly(l, r, v, 2 * x + 1, lx, mid) +
-                   lessOnly(l, r, v, 2 * x + 2, mid, rx);
-        }
-        int lessOnly(int l, int r, ll v) {
-            return lessOnly(l, r, v, 0, 0, size);
-        }
-
-        int lessEqual(int l, int r, ll v, int x, int lx, int rx) {
-            if (lx >= r || rx <= l) return 0;
-            if (lx >= l && rx <= r) {
-                return int(upper_bound(tree[x].begin(), tree[x].end(), v) - tree[x].begin());
-            }
-            int mid = (lx + rx) / 2;
-            return lessEqual(l, r, v, 2 * x + 1, lx, mid) +
-                   lessEqual(l, r, v, 2 * x + 2, mid, rx);
-        }
-        int lessEqual(int l, int r, ll v) {
-            return lessEqual(l, r, v, 0, 0, size);
-        }
-        int greaterEqual(int l, int r, ll v) {
-            return (r - l) - lessOnly(l, r, v, 0, 0, size);
-        }
-        int greaterOnly(int l, int r, ll v) {
-            return (r - l) - lessEqual(l, r, v, 0, 0, size);
-        }
-    };
-*/
+// // 0-index and Not Include R
+// /*
+//     struct MergeSortTrees {
+//         int size;
+//         vector<vector<ll>> tree;
+//         explicit MergeSortTrees(const vector<ll>& a) {
+//             size = a.size();
+//             tree.resize(4 * size);
+//             build(0, 0, size, a);
+//         }
+//         void build(int x, int lx, int rx, const vector<ll>& a) {
+//             if (rx - lx == 1) {
+//                 tree[x] = {a[lx]};
+//                 return;
+//             }
+//             int mid = (lx + rx) / 2;
+//             build(2 * x + 1, lx, mid, a);
+//             build(2 * x + 2, mid, rx, a);
+//             tree[x].resize(tree[2 * x + 1].size() + tree[2 * x + 2].size());
+//             merge(tree[2 * x + 1].begin(), tree[2 * x + 1].end(),
+//                  tree[2 * x + 2].begin(), tree[2 * x + 2].end(),
+//                  tree[x].begin());
+//         }
+//         int lessOnly(int l, int r, ll v, int x, int lx, int rx) {
+//             if (lx >= r || rx <= l) return 0;
+//             if (lx >= l && rx <= r) {
+//                 return int(lower_bound(tree[x].begin(), tree[x].end(), v) - tree[x].begin());
+//             }
+//             int mid = (lx + rx) / 2;
+//             return lessOnly(l, r, v, 2 * x + 1, lx, mid) +
+//                    lessOnly(l, r, v, 2 * x + 2, mid, rx);
+//         }
+//         int lessOnly(int l, int r, ll v) {
+//             return lessOnly(l, r, v, 0, 0, size);
+//         }
+//
+//         int lessEqual(int l, int r, ll v, int x, int lx, int rx) {
+//             if (lx >= r || rx <= l) return 0;
+//             if (lx >= l && rx <= r) {
+//                 return int(upper_bound(tree[x].begin(), tree[x].end(), v) - tree[x].begin());
+//             }
+//             int mid = (lx + rx) / 2;
+//             return lessEqual(l, r, v, 2 * x + 1, lx, mid) +
+//                    lessEqual(l, r, v, 2 * x + 2, mid, rx);
+//         }
+//         int lessEqual(int l, int r, ll v) {
+//             return lessEqual(l, r, v, 0, 0, size);
+//         }
+//         int greaterEqual(int l, int r, ll v) {
+//             return (r - l) - lessOnly(l, r, v, 0, 0, size);
+//         }
+//         int greaterOnly(int l, int r, ll v) {
+//             return (r - l) - lessEqual(l, r, v, 0, 0, size);
+//         }
+//     };
+// */
 }
 namespace SparseTable {
     // struct Sparse {
